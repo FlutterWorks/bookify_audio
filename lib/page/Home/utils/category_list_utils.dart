ae5f86f25d4d9add0778faf12994e24d8e744c01
Md Apon Ahmed <mdaponahmed412@gmail.com>
2024-08-23 15:02:41 +0600

    i hope category solve

--- lib/page/Home/utils/category_list_utils.dart
+++ lib/page/Home/utils/category_list_utils.dart
@@ -1,114 +1,8 @@
-// import 'dart:convert';
-// import 'package:http/http.dart' as http;
-// import 'package:flutter/material.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-// import 'package:test/page/Home/screen/see_more.dart';
-// import 'package:test/page/Home/widget/category_button_widget.dart';
-
-// class CategoryListUtils extends StatefulWidget {
-//   const CategoryListUtils({super.key});
-
-//   @override
-//   State<CategoryListUtils> createState() => _CategoryListUtilsState();
-// }
-
-// class _CategoryListUtilsState extends State<CategoryListUtils> {
-//   List<dynamic> categoryList = [];
-//   bool isLoading = true;
-//   dynamic selectedPerson;
-
-//   Future<void> getData() async {
-//     final res = await http.get(
-//       Uri.parse('https://apon06.github.io/bookify_api/category.json'),
-//     );
-//     if (res.statusCode == 200) {
-//       final decoded = json.decode(res.body);
-//       setState(() {
-//         categoryList = decoded['Category'];
-//         isLoading = false;
-//         selectedPerson = categoryList.isNotEmpty ? categoryList[0] : null;
-//       });
-//       await saveDataToPreferences();
-//     } else {
-//       setState(() {
-//         isLoading = false;
-//       });
-//     }
-//   }
-
-//   Future<void> saveDataToPreferences() async {
-//     final prefs = await SharedPreferences.getInstance();
-//     prefs.setString('CategorySaveKey', json.encode(categoryList));
-//   }
-
-//   Future<void> loadData() async {
-//     final prefs = await SharedPreferences.getInstance();
-//     final savedData = prefs.getString('CategorySaveKey');
-//     if (savedData != null) {
-//       setState(() {
-//         categoryList = json.decode(savedData);
-//         selectedPerson = categoryList.isNotEmpty ? categoryList[0] : null;
-//         isLoading = false;
-//       });
-//     }
-//     await getData();
-//   }
-
-//   Future<void> refreshData() async {
-//     setState(() {
-//       isLoading = true;
-//     });
-//     await getData();
-//   }
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     loadData();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SizedBox(
-//       height: 32,
-//       child: ListView.builder(
-//         scrollDirection: Axis.horizontal,
-//         itemCount: categoryList.length,
-//         itemBuilder: (b, index) {
-//           var categoryApi = categoryList[index];
-//           // print(categoryApi["bookImage"]); // image but why plz tell me
-//           return CategoryButtonWidget(
-//             onTap: () => Navigator.push(
-//               context,
-//               MaterialPageRoute(
-//                 builder: (context) => SeeMorePage(
-//                   api: categoryApi["api"],
-//                   bookType: categoryApi["bookType"],
-//                   bookImage: categoryApi["bookImage"],
-//                   bookName: categoryApi["bookName"],
-//                   bookCreatorName: categoryApi["bookCreatorName"],
-//                   // api: 'https://apon06.github.io/bookify_api/dummy_api.json',
-//                   // bookType: 'রবীন্দ্রনাথ ঠাকুর',
-//                   // bookImage: "bookImage",
-//                   // bookCreatorName: 'bookCreatorName',
-//                   // bookName: "রবীন্দ্রনাথ ঠাকুর",
-//                 ),
-//               ),
-//             ),
-//             categoryText: categoryApi["bookType"],
-//             categoryColor: categoryApi["color"],
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
-
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:test/page/Home/screen/category_list_see_more_page.dart';
+import 'package:test/page/Home/screen/see_more.dart';
 import 'package:test/page/Home/widget/category_button_widget.dart';
 
 class CategoryListUtils extends StatefulWidget {
@@ -121,7 +15,7 @@ class CategoryListUtils extends StatefulWidget {
 class _CategoryListUtilsState extends State<CategoryListUtils> {
   List<dynamic> categoryList = [];
   bool isLoading = true;
-  dynamic selectedPerson;
+  // dynamic selectedPerson;
 
   Future<void> getData() async {
     final res = await http.get(
@@ -129,20 +23,16 @@ class _CategoryListUtilsState extends State<CategoryListUtils> {
     );
     if (res.statusCode == 200) {
       final decoded = json.decode(res.body);
-      if (mounted) {
-        setState(() {
-          categoryList = decoded['Category'];
-          isLoading = false;
-          selectedPerson = categoryList.isNotEmpty ? categoryList[0] : null;
-        });
-        await saveDataToPreferences();
-      }
+      setState(() {
+        categoryList = decoded['Category'];
+        isLoading = false;
+        // selectedPerson = categoryList.isNotEmpty ? categoryList[0] : null;
+      });
+      await saveDataToPreferences();
     } else {
-      if (mounted) {
-        setState(() {
-          isLoading = false;
-        });
-      }
+      setState(() {
+        isLoading = false;
+      });
     }
   }
 
@@ -155,23 +45,19 @@ class _CategoryListUtilsState extends State<CategoryListUtils> {
     final prefs = await SharedPreferences.getInstance();
     final savedData = prefs.getString('CategorySaveKey');
     if (savedData != null) {
-      if (mounted) {
-        setState(() {
-          categoryList = json.decode(savedData);
-          selectedPerson = categoryList.isNotEmpty ? categoryList[0] : null;
-          isLoading = false;
-        });
-      }
+      setState(() {
+        categoryList = json.decode(savedData);
+        // selectedPerson = categoryList.isNotEmpty ? categoryList[0] : null;
+        isLoading = false;
+      });
     }
     await getData();
   }
 
   Future<void> refreshData() async {
-    if (mounted) {
-      setState(() {
-        isLoading = true;
-      });
-    }
+    setState(() {
+      isLoading = true;
+    });
     await getData();
   }
 
@@ -179,11 +65,6 @@ class _CategoryListUtilsState extends State<CategoryListUtils> {
   void initState() {
     super.initState();
     loadData();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
   }
 
   @override
@@ -195,17 +76,17 @@ class _CategoryListUtilsState extends State<CategoryListUtils> {
         itemCount: categoryList.length,
         itemBuilder: (b, index) {
           var categoryApi = categoryList[index];
+          // print(categoryApi["bookImage"]); // image but why plz tell me
           return CategoryButtonWidget(
             onTap: () => Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => CategoryListSeeMorePage(
+                builder: (context) => SeeMorePage(
                   api: categoryApi["api"],
-                  bookType: categoryApi["bookType"],
                   bookImage: categoryApi["bookImage"],
                   bookName: categoryApi["bookName"],
                   bookCreatorName: categoryApi["bookCreatorName"],
-                  saveKey: 'categorySave',
+                  saveKey: categoryApi["saveKey"],
                 ),
               ),
             ),
