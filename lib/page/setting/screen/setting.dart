56d408c17cd7c8275b58984500b7cde1be74b62f
Md Apon Ahmed <mdaponahmed412@gmail.com>
2024-09-29 21:32:31 +0600

    same change and update

--- lib/page/setting/screen/setting.dart
+++ lib/page/setting/screen/setting.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:test/page/setting/screen/app_information_page.dart';
 import 'package:test/page/setting/screen/change_log_page.dart';
@@ -49,116 +50,146 @@ class _SettingPageState extends State<SettingPage> {
       // bottomNavigationBar: bannerAds != null
       //     ? SizedBox(height: 60, child: StartAppBanner(bannerAds!))
       //     : const SizedBox(),
-      body: Column(
-        children: [
-          Card(
-            child: ListTile(
-              title: const Text('Theme'),
-              trailing: Switch(
-                value: themeProvider.isDarkMode,
-                onChanged: (value) {
-                  themeProvider.toggleTheme(value);
-                },
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Card(
+              child: ListTile(
+                title: const Text('Theme'),
+                trailing: Switch(
+                  value: themeProvider.isDarkMode,
+                  onChanged: (value) {
+                    themeProvider.toggleTheme(value);
+                  },
+                ),
               ),
             ),
-          ),
-          Card(
-            child: ListTile(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (b) => const AppInformationPage(),
-                  ),
-                );
-              },
-              title: const Text('App Information'),
-              trailing: const Icon(Icons.info_rounded),
+            Card(
+              child: ListTile(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (b) => const AppInformationPage(),
+                    ),
+                  );
+                },
+                title: const Text('App Information'),
+                trailing: const Icon(Icons.info_rounded),
+              ),
             ),
-          ),
-          const BookifyAds(
-            apiUrl:
-                'https://apon06.github.io/bookify_api/ads/setting_1.json',
-          ),
-          Card(
-            child: ListTile(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (b) => const ChangeLogPage(),
-                  ),
-                );
-              },
-              title: const Text('Changelog'),
-              trailing: const Icon(Icons.history),
+            const BookifyAds(
+              apiUrl: 'https://apon06.github.io/bookify_api/ads/setting_1.json',
             ),
-          ),
-          // Card(
-          //   child: ListTile(
-          //     onTap: () {
-          //       Navigator.of(context).push(
-          //         MaterialPageRoute(
-          //           builder: (b) => const AboutBookify(),
-          //         ),
-          //       );
-          //     },
-          //     title: const Text('About Bookify'),
-          //     trailing: const Icon(Icons.info_rounded),
-          //   ),
-          // ),
-          Card(
-            child: ListTile(
-              onTap: () async {
-                String privacyUrl =
-                    'https://sites.google.com/view/bookify-audio/home';
-                final Uri url = Uri.parse(privacyUrl);
-                if (await canLaunch(url.toString())) {
-                  await launch(url.toString());
-                } else {
-                  await launch(url.toString());
-                }
-              },
-              title: const Text('Privacy Policy'),
-              trailing: const Icon(Icons.privacy_tip),
+            Card(
+              child: ListTile(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (b) => const ChangeLogPage(),
+                    ),
+                  );
+                },
+                title: const Text('Changelog'),
+                trailing: const Icon(Icons.history),
+              ),
             ),
-          ),
-          const BookifyAds(
-            apiUrl:
-                'https://apon06.github.io/bookify_api/ads/setting_2.json',
-          ),
+            // Card(
+            //   child: ListTile(
+            //     onTap: () {
+            //       Navigator.of(context).push(
+            //         MaterialPageRoute(
+            //           builder: (b) => const AboutBookify(),
+            //         ),
+            //       );
+            //     },
+            //     title: const Text('About Bookify'),
+            //     trailing: const Icon(Icons.info_rounded),
+            //   ),
+            // ),
+            Card(
+              child: ListTile(
+                onTap: () async {
+                  String privacyUrl =
+                      'https://sites.google.com/view/bookify-audio/home';
+                  final Uri url = Uri.parse(privacyUrl);
+                  if (await canLaunch(url.toString())) {
+                    await launch(url.toString());
+                  } else {
+                    await launch(url.toString());
+                  }
+                },
+                title: const Text('Privacy Policy'),
+                trailing: const Icon(Icons.privacy_tip),
+              ),
+            ),
+            const BookifyAds(
+              apiUrl: 'https://apon06.github.io/bookify_api/ads/setting_2.json',
+            ),
 
-          Card(
-            child: ListTile(
-              onTap: () async {
-                String privacyUrl = 'https://t.me/+iCD-EPh5ye1iM2Q1';
-                final Uri url = Uri.parse(privacyUrl);
-                if (await canLaunch(url.toString())) {
-                  await launch(url.toString());
-                } else {
-                  await launch(url.toString());
-                }
-              },
-              title: const Text('Telegram Group'),
-              trailing: const Icon(Icons.telegram),
+            Card(
+              child: ListTile(
+                onTap: () async {
+                  String privacyUrl = 'https://t.me/+iCD-EPh5ye1iM2Q1';
+                  final Uri url = Uri.parse(privacyUrl);
+                  if (await canLaunch(url.toString())) {
+                    await launch(url.toString());
+                  } else {
+                    await launch(url.toString());
+                  }
+                },
+                title: const Text('Telegram Group'),
+                trailing: const Icon(Icons.telegram),
+              ),
             ),
-          ),
-          Card(
-            child: ListTile(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (b) => const MissingStory(),
-                  ),
-                );
-              },
-              title: const Text('Missing Story'),
-              trailing: const Icon(Icons.add),
+            Card(
+              child: ListTile(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (b) => const MissingStory(),
+                    ),
+                  );
+                },
+                title: const Text('Missing Story'),
+                trailing: const Icon(Icons.add),
+              ),
             ),
-          ),
-          const BookifyAds(
-            apiUrl:
-                'https://apon06.github.io/bookify_api/ads/setting_3.json',
-          ),
-        ],
+            const BookifyAds(
+              apiUrl: 'https://apon06.github.io/bookify_api/ads/setting_3.json',
+            ),
+            Card(
+              child: ListTile(
+                onTap: () async {
+                  String privacyUrl =
+                      'https://sites.google.com/view/bookify-term/home';
+                  final Uri url = Uri.parse(privacyUrl);
+                  if (await canLaunch(url.toString())) {
+                    await launch(url.toString());
+                  } else {
+                    await launch(url.toString());
+                  }
+                },
+                title: const Text('Terms & Conditions'),
+                trailing: const Icon(Icons.assignment),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                onTap: () async {
+                  String privacyUrl = 'https://github.com/apon06';
+                  final Uri url = Uri.parse(privacyUrl);
+                  if (await canLaunch(url.toString())) {
+                    await launch(url.toString());
+                  } else {
+                    await launch(url.toString());
+                  }
+                },
+                title: const Text('Github'),
+                trailing: const Icon(FontAwesomeIcons.github),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
